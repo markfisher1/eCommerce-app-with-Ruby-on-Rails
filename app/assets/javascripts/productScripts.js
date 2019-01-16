c5687ecbf354c00d6cd084c11f5c7bbb6ddd0873
Alphabetus <dlgreis88@gmail.com>
2019-01-16 12:48:36 +0100

    [v0.6.1] > Fix pipeline asset
    - Fix javascript syntax to face asset pipeline requirements and avoid errors.

--- app/assets/javascripts/productScripts.js
+++ app/assets/javascripts/productScripts.js
@@ -12,6 +12,11 @@ $(document).ready(function(){
   var productArray = $("#hidden_information_products_array").data("products-array");
 
   for (i = 0; i < productArray.length; i++){
+    // NOTE:  I was using const instead of var, however Asset compilation was
+    //        failing when deploying to Heroku.
+    //        The error message on terminal pointed to this line and to the
+    //        keyword `const` flagging some kind of error while performing the
+    //        uglifier process.
     var imageObj = $("img[data-product-id='"+productArray[i].id+"']");
     var downloadingImage = new Image();
     downloadingImage.src = "/assets/" + productArray[i].image_url;
