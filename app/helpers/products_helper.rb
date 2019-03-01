1c95650d9d3914e2ff28b5bcb164de183b65d213
Alphabetus <dlgreis88@gmail.com>
2019-03-01 14:16:29 +0100

    [v0.38.2 - 01/mar/19] > HotFix > Attempt to reduce memory usage from redis on heroku

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -8,4 +8,34 @@ module ProductsHelper
     end
   end
 
+
+  # LOL heroku cant handle my cache... lets hide something from them
+  def getProdCache
+
+    if Rails.env.development?
+      if $redis.exists("product-list-cache")
+
+        return "#{$redis.get("product-list-cache").html_safe}"
+
+        logger.debug("[DOBBY] > CACHE OK > PRODUCTS LOADED")
+        logger.debug("[DOBBY] > TTL REMAINING: #{$redis.ttl("product-list-cache")}")
+
+      else
+
+        content = render @products
+        $redis.set("product-list-cache", content)
+        $redis.expire("product-list-cache", 300)
+        return "#{$redis.get("product-list-cache").html_safe}"
+
+        logger.debug("[DOBBY] > CACHE NOT OK > PRODUCTS CACHED")
+        logger.debug("[DOBBY] > TTL REMAINING: #{$redis.ttl("product-list-cache")}")
+
+      end
+    else
+
+      return render @products
+
+    end
+
+  end
 end
